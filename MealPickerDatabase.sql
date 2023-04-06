3e54ba0913a9192b1245c14761a861bfc4dcb7a4
anneliispoll <anneliis.poll@gmail.com>
2023-04-06 18:35:27 +0300

    Database update

--- MealPickerDatabase.sql
+++ MealPickerDatabase.sql
@@ -8,14 +8,14 @@ CREATE TABLE meals (
 
 CREATE TABLE seasons (
   SeasonID INT NOT NULL AUTO_INCREMENT,
-  SeasonName VARCHAR(255) NOT NULL,
+  season_name VARCHAR(255) NOT NULL,
   PRIMARY KEY (SeasonID)
 );
 
 CREATE TABLE meal_times (
   MealTimeID INT NOT NULL AUTO_INCREMENT,
-  MealTimeName VARCHAR(255) NOT NULL,
-  PRIMARY KEY (MealTimeID)
+  meal_time_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (meal_timeid)
 );
 
 CREATE TABLE meal_seasons (
@@ -28,22 +28,22 @@ CREATE TABLE meal_seasons (
 
 CREATE TABLE meal_meal_times (
   MealID INT NOT NULL,
-  MealTimeID INT NOT NULL,
-  PRIMARY KEY (MealID, MealTimeID),
+  meal_timeid INT NOT NULL,
+  PRIMARY KEY (MealID, meal_timeid),
   FOREIGN KEY (MealID) REFERENCES meals(MealID),
-  FOREIGN KEY (MealTimeID) REFERENCES meal_times(MealTimeID)
+  FOREIGN KEY (meal_timeid) REFERENCES meal_times(meal_timeid)
 );
 
 -- Inserting the seasons --
-INSERT INTO seasons (SeasonName) VALUES ('spring');
-INSERT INTO seasons (SeasonName) VALUES ('summer');
-INSERT INTO seasons (SeasonName) VALUES ('autumn');
-INSERT INTO seasons (SeasonName) VALUES ('winter');
+INSERT INTO seasons (season_name) VALUES ('spring');
+INSERT INTO seasons (season_name) VALUES ('summer');
+INSERT INTO seasons (season_name) VALUES ('autumn');
+INSERT INTO seasons (season_name) VALUES ('winter');
 
 -- inserting the meal_times --
-INSERT INTO meal_times (MealTimeName) VALUES ('breakfast');
-INSERT INTO meal_times (MealTimeName) VALUES ('lunch');
-INSERT INTO meal_times (MealTimeName) VALUES ('dinner');
+INSERT INTO meal_times (meal_time_name) VALUES ('breakfast');
+INSERT INTO meal_times (meal_time_name) VALUES ('lunch');
+INSERT INTO meal_times (meal_time_name) VALUES ('dinner');
 
 
 -- inserting meals --
@@ -62,57 +62,55 @@ INSERT INTO meals (meal_name) VALUES ('Casserole');
 
 -- inserting into meal_seasons for the season--
 INSERT INTO meal_seasons (MealID, SeasonID) VALUES
-  (1, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (1, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (1, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (2, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (2, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (2, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (2, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (3, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (4, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (4, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (4, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (4, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (5, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (5, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (5, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (5, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (6, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (6, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (6, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (6, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (7, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (7, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (7, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (8, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (8, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (9, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter')),
-  (9, (SELECT SeasonID FROM seasons WHERE SeasonName = 'autumn')),
-  (9, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (10, (SELECT SeasonID FROM seasons WHERE SeasonName = 'spring')),
-  (10, (SELECT SeasonID FROM seasons WHERE SeasonName = 'summer')),
-  (11, (SELECT SeasonID FROM seasons WHERE SeasonName = 'winter'));
+  (1, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (1, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (1, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (2, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (2, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (2, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (2, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (3, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (4, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (4, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (4, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (4, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (5, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (5, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (5, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (5, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (6, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (6, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (6, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (6, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (7, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (7, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (7, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (8, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (8, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (9, (SELECT SeasonID FROM seasons WHERE season_name = 'winter')),
+  (9, (SELECT SeasonID FROM seasons WHERE season_name = 'autumn')),
+  (9, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (10, (SELECT SeasonID FROM seasons WHERE season_name = 'spring')),
+  (10, (SELECT SeasonID FROM seasons WHERE season_name = 'summer')),
+  (11, (SELECT SeasonID FROM seasons WHERE season_name = 'winter'));
 
 -- inserting into meal_meal_times for the mealtime--
-INSERT INTO meal_meal_times (MealID, MealTimeID) VALUES
-(1, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'breakfast')),
-(2, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'breakfast')),
-(3, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'breakfast')),
-(4, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'breakfast')),
-(5, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'breakfast')),
-(6, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(6, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner')),
-(7, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(7, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner')),
-(8, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(8, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner')),
-(11, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(9, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner')),
-(9, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(10, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'lunch')),
-(10, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner')),
-(11, (SELECT MealTimeID FROM meal_times WHERE MealTimeName = 'dinner'));
-  
+INSERT INTO meal_meal_times (MealID, meal_timeid) VALUES
+(1, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'breakfast')),
+(2, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'breakfast')),
+(3, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'breakfast')),
+(4, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'breakfast')),
+(5, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'breakfast')),
+(6, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'lunch')),
+(6, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner')),
+(7, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'lunch')),
+(7, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner')),
+(8, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'lunch')),
+(8, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner')),
+(9, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner')),
+(9, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'lunch')),
+(10, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'lunch')),
+(10, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner')),
+(11, (SELECT meal_timeid FROM meal_times WHERE meal_time_name = 'dinner'));
 
 
